bd36df7a1e4beba69c343a18c5857da29d2639b0
ShneorAziza <shneoraziza@gmail.com>
2024-08-11 22:49:58 +0300

    Login 0.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 function App() {
   return (
     <>
-      <HomePage />
+      <Login />
     </>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,29 @@
+import react from "react"
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="UserName" required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+        </div>
+        
+        <div className="remember-forget">
+          <label><input type="checkbox" />remember me</label>
+          <a href="#">forget password?</a>
+        </div>
+      
+      <button type="submit">Login</button>
+      
+      <div className="register-link">
+        <p>Your first time hear? <a href="#">Register</a></p>
+      </div>
+      </form>
+    </div>
   )
 }
 
