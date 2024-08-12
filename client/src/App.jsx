6954f438b4954c5112b72e933a5ca38a122cf6f5
ShneorAziza <shneoraziza@gmail.com>
2024-08-12 15:26:54 +0300

    register 0.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <>
       <Login />
+      <RegisterPage />
     </>
   );
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,6 +1,31 @@
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+     <div className="wrapper">
+    <form action="">
+      <h1>Sign in</h1>
+      <div className="input-box">
+        <input type="text" placeholder="First Name" required />
+      </div>
+      <div className="input-box">
+        <input type="text" placeholder="Last Name" required />
+      </div>
+      <div className="input-box">
+        <input type="email" placeholder="Email" required />
+      </div>
+      <div className="input-box">
+        <input type="text" placeholder="UserName" required />
+      </div>
+      <div className="input-box">
+        <input type="password" placeholder="Password" required />
+      </div>
+    
+    <button type="submit">Sign in</button>
+    
+    <div className="login-link">
+      <p>Not your first time hear? <a href="#">Login</a></p>
+    </div>
+    </form>
+  </div>
   )
 }
 
